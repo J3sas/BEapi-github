39deceeed25acc84ab4ecd739b27f9f278bcde50
Your Name <you@example.com>
2022-06-07 18:42:13 +0800

    update nwd schema

--- models/nwd-user-schema.js
+++ models/nwd-user-schema.js
@@ -5,12 +5,60 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name : {
         type : String,
-        required : true
+        required : true,
     },
     address : {
         type :String,
         required : true
+    },
+    accountNum : {
+        type :String,
+        required : true
+    },
+    rateClass : {
+        type :String,
+        required : true
+    },
+    meterNum : {
+        type :String,
+        required : true
+    },
+    prevRdgDate : {
+        type :String
+    },
+    balancePrevBill : {
+        type :Number,
+        default : 0
+    },
+    totalCurrBill : {
+        type :Number,
+        default : 0
+    },
+    accountType : {
+        type :String,
+        required : true
+    },
+    billInfo : {
+        type :Array
+    },
+    prevReading : {
+        type :Number,
+        default : 0
+    },
+    hasActiveBill : {
+        type :Boolean,
+        default : false
+    },
+    hasReqPayment : {
+        type :Boolean,
+        default : false
+    },
+    paidBillInfo : {
+        type :Array
     }
 });
+
+
+
 const User = mongoose.model("nwdUsers",userSchema); // table name should be small letters
 module.exports = User;
